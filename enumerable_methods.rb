05e6d36b9cfe007a693d23f5b62cd35fdc3c1f1f
sick-carlito <carlb1319@gmail.com>
2019-10-25 00:01:55 +0300

    modified main file

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -2,11 +2,11 @@
 
 # My comment
 module Enumerable
-	def my_each 
-	  for element in self
-		yield(element)
-	  end
-	  self
+  def my_each 
+	for element in self
+	 yield(element)
+	end
+	 self
 	end
 
 	def my_each_with_index
